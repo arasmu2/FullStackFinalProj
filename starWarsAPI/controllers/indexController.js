0be7d38177f3f55d1c48174eb272d1792500c74a
emrsmssn <aaron.rasmussen@gmail.com>
2022-02-28 17:48:09 -0800

    custom module/class for parsing quote text

--- starWarsAPI/controllers/indexController.js
+++ starWarsAPI/controllers/indexController.js
@@ -2,16 +2,18 @@ const { body, validationResult } = require("express-validator");
 
 var async = require("async");
 var axios = require("axios");
+var Parser = require("../public/rndMeme").getInstance();
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 exports.index = async function (req, res) {
   let response, data, err, img_response, img_url, img_err;
+  // fetch a random quote
   try {
     response = await axios.get(
       "http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote"
     );
-    data = response.data;
-    console.log(data.content);
+    data = response.data.content;
+    console.log(data);
   } catch (error) {
     console.group("GET quote err");
     console.error(error);
@@ -19,23 +21,26 @@ exports.index = async function (req, res) {
     err = error;
   }
 
+  let captions = Parser.parseQuote(data);
+  captions = ["we live in", "a society"];
+
   try {
     img_response = await axios.post(
       "https://api.imgflip.com/caption_image",
       {},
       {
         params: {
-          template_id: "1373425",
+          template_id: "14371066",
           username: "project_acct",
           password: "insecurePW",
-          text0: "we live in a society",
-          text1: "bottom text",
-          max_font_size: "20",
+          text0: captions[0],
+          text1: captions[1],
+          max_font_size: "30",
         },
       }
     );
     img_url = img_response.data.data.url;
-    console.log(img_response.data);
+    //console.log(img_response.data);
   } catch (error) {
     console.group("POST meme err");
     console.error(error);
@@ -43,5 +48,5 @@ exports.index = async function (req, res) {
     img_err = error;
   }
 
-  res.render("index", { results: data.content, err: err, meme: img_url });
+  res.render("index", { results: data, err: err, meme: img_url });
 };

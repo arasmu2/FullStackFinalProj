ce5b704601310a1d99f22d0ac3ec08e3833ac65f
Mikayla Campbell <campbell.mikaylaj@gmail.com>
2022-03-02 22:36:45 -0800

    Adding movie modal api data

--- starWarsAPI/controllers/moviesController.js
+++ starWarsAPI/controllers/moviesController.js
@@ -8,13 +8,19 @@ exports.movies = function(req, res, next) {
     .then(result => result.json())
     .then((output) => {
         var movieData = [];
+        var extraData = [];
         for (let i = 0; i < output["result"].length; i++)
         {
-            movieData.push(output["result"][i]["properties"]["title"])
+            movieData.push(output["result"][i]["properties"]["title"]);
+            extraData.push(["Producer(s): " + output["result"][i]["properties"]["producer"] + '\n',
+                "Director(s): " + output["result"][i]["properties"]["director"] + '\n',
+                "Release Date: " + output["result"][i]["properties"]["release_date"] + '\n',
+                "Opening Crawl: " + output["result"][i]["properties"]["opening_crawl"] + '\n']);
         }
         res.render('lists', {
             title: 'Movies',
-            data: movieData
+            data: movieData,
+            extra: extraData
         });
     })
     .catch(err => console.error(err));

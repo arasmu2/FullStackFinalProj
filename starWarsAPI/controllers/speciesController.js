8f83395674b85aeab3eada4572650e431edeb41b
Mikayla Campbell <campbell.mikaylaj@gmail.com>
2022-03-04 16:13:43 -0800

    Fixed number of species

--- starWarsAPI/controllers/speciesController.js
+++ starWarsAPI/controllers/speciesController.js
@@ -6,37 +6,31 @@ var async = require('async');
 exports.species = function(req, res, next) {
     var speciesData = [];
     var extraData = [];
+    var promises = [];
 
-    Promise.all([
-        fetch('https://www.swapi.tech/api/species/1').then(result => result.json()),
-        fetch('https://www.swapi.tech/api/species/2').then(result => result.json()),
-        fetch('https://www.swapi.tech/api/species/3').then(result => result.json()),
-        fetch('https://www.swapi.tech/api/species/4').then(result => result.json()),
-        fetch('https://www.swapi.tech/api/species/5').then(result => result.json()),
-        fetch('https://www.swapi.tech/api/species/6').then(result => result.json()),
-        fetch('https://www.swapi.tech/api/species/7').then(result => result.json()),
-        fetch('https://www.swapi.tech/api/species/8').then(result => result.json()),
-        fetch('https://www.swapi.tech/api/species/9').then(result => result.json()),
-        fetch('https://www.swapi.tech/api/species/10').then(result => result.json())
-        ]).then((output) => {
-            for (let i = 0; i < 10; i++)
-            {
-                speciesData.push(output[i]["result"]["properties"]["name"]);
-                
-                extraData.push(["Classification: " + output[i]["result"]["properties"]["classification"] + '\n',
-                    "Designation: " + output[i]["result"]["properties"]["designation"] + '\n',
-                    "Average Height: " + output[i]["result"]["properties"]["average_height"] + '\n',
-                    "Average Lifespan: " + output[i]["result"]["properties"]["average_lifespan"] + '\n',
-                    "Hair Color(s): " + output[i]["result"]["properties"]["hair_colors"] + '\n',
-                    "Eye Color(s): " + output[i]["result"]["properties"]["eye_colors"] + '\n',
-                    "Language: " + output[i]["result"]["properties"]["language"] + '\n']);
-            }
-            res.render('lists', {
-                title: 'Species',
-                data: speciesData,
-                extra: extraData
-            });
+    for (let j = 1; j < 38; j++)
+    {
+        url = 'https://www.swapi.tech/api/species/' + j;
+        promises.push(fetch(url).then(result => result.json()));
+    }
 
+    Promise.all(promises).then((output) => {
+        for (let i = 0; i < 10; i++)
+        {
+            speciesData.push(output[i]["result"]["properties"]["name"]);
+            
+            extraData.push(["Classification: " + output[i]["result"]["properties"]["classification"] + '\n',
+                "Designation: " + output[i]["result"]["properties"]["designation"] + '\n',
+                "Average Height: " + output[i]["result"]["properties"]["average_height"] + '\n',
+                "Average Lifespan: " + output[i]["result"]["properties"]["average_lifespan"] + '\n',
+                "Hair Color(s): " + output[i]["result"]["properties"]["hair_colors"] + '\n',
+                "Eye Color(s): " + output[i]["result"]["properties"]["eye_colors"] + '\n',
+                "Language: " + output[i]["result"]["properties"]["language"] + '\n']);
+        }
+        res.render('lists', {
+            title: 'Species',
+            data: speciesData,
+            extra: extraData
+        });
     });
-
 };

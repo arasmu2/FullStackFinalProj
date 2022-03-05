be2e17e984c20d3976b10c12987dbad209536ee1
Mikayla Campbell <campbell.mikaylaj@gmail.com>
2022-03-04 20:50:25 -0800

    Reduced API calls

--- starWarsAPI/controllers/planetsController.js
+++ starWarsAPI/controllers/planetsController.js
@@ -2,37 +2,45 @@ const { body,validationResult } = require('express-validator');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 var async = require('async');
+var planetData = [];
+var extraData = [];
 
 exports.planets = function(req, res, next) {
-    var planetData = [];
-    var extraData = [];
     var promises = [];
 
-    for (let j = 1; j < 61; j++)
-    {
-        url = 'https://www.swapi.tech/api/planets/' + j;
-        promises.push(fetch(url).then(result => result.json()));
-    }
-
-    Promise.all(promises).then((output) => {
-        for (let i = 0; i < 60; i++)
+    if (planetData.length === 0) {
+        for (let j = 1; j < 61; j++)
         {
-            planetData.push(output[i]["result"]["properties"]["name"]);
-            
-            extraData.push(["Diameter: " + output[i]["result"]["properties"]["diameter"] + '\n',
-                "Rotation Period: " + output[i]["result"]["properties"]["rotation_period"] + '\n',
-                "Orbital Period: " + output[i]["result"]["properties"]["orbital_period"] + '\n',
-                "Gravity: " + output[i]["result"]["properties"]["gravity"] + '\n',
-                "Population: " + output[i]["result"]["properties"]["population"] + '\n',
-                "Climate(s): " + output[i]["result"]["properties"]["climate"] + '\n',
-                "Terrain(s): " + output[i]["result"]["properties"]["terrain"] + '\n',
-                "Surface Water: " + output[i]["result"]["properties"]["surface_water"] + '\n']);
+            url = 'https://www.swapi.tech/api/planets/' + j;
+            promises.push(fetch(url).then(result => result.json()));
         }
-        console.log(planetData);
+
+        Promise.all(promises).then((output) => {
+            for (let i = 0; i < 60; i++)
+            {
+                planetData.push(output[i]["result"]["properties"]["name"]);
+                
+                extraData.push(["Diameter: " + output[i]["result"]["properties"]["diameter"] + '\n',
+                    "Rotation Period: " + output[i]["result"]["properties"]["rotation_period"] + '\n',
+                    "Orbital Period: " + output[i]["result"]["properties"]["orbital_period"] + '\n',
+                    "Gravity: " + output[i]["result"]["properties"]["gravity"] + '\n',
+                    "Population: " + output[i]["result"]["properties"]["population"] + '\n',
+                    "Climate(s): " + output[i]["result"]["properties"]["climate"] + '\n',
+                    "Terrain(s): " + output[i]["result"]["properties"]["terrain"] + '\n',
+                    "Surface Water: " + output[i]["result"]["properties"]["surface_water"] + '\n']);
+            }
+            res.render('lists', {
+                title: 'Planets',
+                data: planetData,
+                extra: extraData
+            });
+        });
+    }
+    else {
         res.render('lists', {
             title: 'Planets',
             data: planetData,
             extra: extraData
         });
-    });
+    }
 };

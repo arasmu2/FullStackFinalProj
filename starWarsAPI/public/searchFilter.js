2c20bf29bdcbbe3fb0ada53d1afd815fe425a156
iliaspdx <ilias@pdx.edu>
2022-03-07 14:34:01 -0800

    Made changes. update styles, updated names, updated pug files

--- starWarsAPI/public/searchFilter.js
+++ starWarsAPI/public/searchFilter.js
@@ -1,20 +1,15 @@
 // https://www.w3schools.com/howto/howto_js_filter_lists.asp
-function myFunction() {
+function searchBar() {
     // Declare variables
-    console.log("Hello There");
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById('searchFilter');
     filter = input.value.toUpperCase();
     ul = document.getElementById('order-list-container');
     li = ul.getElementsByTagName('li');
 
-
-    // a = li[0].getElementsByTagName("div")[0].getElementsByTagName("div")[0];
-    // console.log(a.innerText);
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
-      //a = li[i].getElementsByTagName("div")[0].getElementsByTagName("div")[0].getElementsByTagName("p")[0];
-      a = li[i].getElementsByTagName("div")[0].getElementsByTagName("div")[0];
+      a = li[i].getElementsByTagName("div")[0].getElementsByTagName("div")[0].getElementsByTagName("p")[0];
       txtValue = a.textContent || a.innerText;
       // Checks if the subset character (filter) is within txtValue. If not then set display to none
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
